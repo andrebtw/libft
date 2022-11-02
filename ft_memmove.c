79e264f9ebde319d01a35cd5394a79731bdec474
André <gwandrewgd@gmail.com>
2022-11-02 12:20:56 +0100

    Fixed memmove, now it should work.

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,7 +12,7 @@
 
 #include <string.h>
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+static void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
 	size_t	i;
 	char	*string_dest;
@@ -31,30 +31,19 @@ void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
 	char	*string_dest;
-	char	char_src;
 	const char	*string_src;
+	int	i;
 
+	i = (int)n - 1;
 	string_dest = dest;
 	string_src = src;
-	i = 0;
-	while (i < n)
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	while (i >= 0)
 	{
-		char_src = string_src[i];
-		string_dest[i] = char_src;
-		i++;
+		string_dest[i] = string_src[i];
+		i--;
 	}
 	return (dest);
-}
-
-#include <stdio.h>
-int	main(void)
-{
-	char str[100] = "Learningisfun";
-	char *dest;
-
-	ft_memmove(str + 8, str, 10);
-	printf("%s", str);
-	return (0);
 }
