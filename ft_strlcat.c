9aaae5e348a793f5abd4ab3b2b28969258381c0d
André <gwandrewgd@gmail.com>
2022-11-02 23:20:07 +0100

    strlcat now works.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,48 +6,53 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 16:35:35 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/11/02 18:07:41 by andre            ###   ########.fr       */
+/*   Updated: 2022/11/02 23:12:48 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+static size_t   	size_count(char *dst, const char *src, size_t size)
 {
-	unsigned int	src_size;
-	unsigned int	dest_size;
+    int    i;
+    int    j;
 
-	src_size = 0;
-	dest_size = 0;
-	while (dst[dest_size] != '\0')
-	{
-		dest_size++;
-		size--;
-	}
-	while (src[src_size] != '\0' && (int)size - 0 > 0)
-	{
-		dst[dest_size] = src[src_size];
-		dest_size++;
-		src_size++;
-		size--;
-	}/*
-	while (src[src_size] != '\0')
-	{
-		dest_size++;
-		src_size++;
-	}*/
-	dst[dest_size] = '\0';
-	return (dest_size);
+    i = 0;
+    j = 0;
+    while (dst[i] != '\0')
+        i++;
+    while (src[j] != '\0')
+        j++;
+    if (i + j > size)
+    {
+        if (i > j)
+            return (j + size);
+        return (i + size);
+    }
+    return (i + j);
 }
-/*
-#include <bsd/string.h>
-#include <stdio.h>
-int	main(void)
+
+size_t    ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char src[20] = "World";
-	char dest[20] = "Hello";
-	size_t num = ft_strlcat(dest, src, 1);
-	printf("Dest : %s\n", dest);
-	printf("size_t returning : %d", num);
-	return (0);
-}*/
+    unsigned int	src_size;
+    unsigned int	dest_size;
+    size_t			return_size;
+
+    return_size = size_count(dst, src, size);
+    src_size = 0;
+    dest_size = 0;
+    while (dst[dest_size] != '\0')
+    {
+        dest_size++;
+        size--;
+    }
+    while (src[src_size] != '\0' && (int)size - 1 > 0)
+    {
+        dst[dest_size] = src[src_size];
+        dest_size++;
+        src_size++;
+        size--;
+    }
+    dst[dest_size] = '\0';
+    return (return_size);
+}
