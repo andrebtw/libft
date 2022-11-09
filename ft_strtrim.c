9605eebce603684d442be7f94b05d1cc6136a815
André <gwandrewgd@gmail.com>
2022-11-09 16:09:08 +0100

    ft_strtrim, not done yet, some progress...

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -47,24 +47,46 @@ static int	ft_skip_begin(char const *s1, char const *set)
 	return (i);
 }
 
-int	ft_skip_end(char const *s1, char const *set)
+static int	ft_skip_end(char const *s1, char const *set, int count)
 {
-	
-}
+	int	i;
+	int	j;
+	int	found_char;
 
+	i = 0;
+	while (s1[count + i] != '\0')
+	{
+		found_char = 0;
+		j = 0;
+		while (set[j] != '\0')
+		{
+			if (s1[count + i] == set[j])
+				found_char = 1;
+			j++;
+		}
+		if (found_char == 0)
+			return (count);
+		i++;
+	}
+	if (s1[count + i] == '\0')
+		return (count + i);
+	return (count);
+}
+#include <stdio.h>
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int	i;
 	int	k;
 	char *r_string;
 
-	r_string = (char *) malloc (ft_strlen_int(s1));
+	r_string = (char *) malloc (ft_strlen_int(s1) + 1);
 	if (r_string == NULL)
 		return (NULL);
 	i = 0;
 	k = 0;
 	while (s1[i] != '\0')
 	{
+		k = ft_skip_end(s1, set, k);
 		if (i == 0)
 			i = ft_skip_begin(s1, set);
 		r_string[k] = s1[i];
@@ -79,7 +101,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 #include <stdio.h>
 int	main(void)
 {
-	char *test = ft_strtrim(" z42 networkzzz z z ", " ");
+	char *test = ft_strtrim("z42 networkzbzzzzzzzzzzzzzzzzzzzz", "z");
 	printf("%s", test);
 	return (0);
 }
