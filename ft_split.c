cff2708ae815f17d91d1b0094d54dae70be70e4e
André <43089870+andrebtw@users.noreply.github.com>
2022-11-15 15:35:58 +0100

    Still works, now good malloc usage, norm still not done.

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,53 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 13:21:06 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/11/15 14:47:17 by anrodri2         ###   ########.fr       */
+/*   Updated: 2022/11/15 15:35:23 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdlib.h>
 #include "libft.h"
+
+static void	ft_free_all(char **string, int i)
+{
+	while (i >= 0)
+	{
+		free(string[i]);
+		i--;
+	}
+	free(string);
+}
+
+static int	ft_isempty(char *string)
+{
+	if (string[0] != 0)
+		return (1);
+	return (0);
+}
+
+static char	**ft_malloced(char const *s)
+{
+	char	**r_string;
+	int		i;
+
+	i = 0;
+	r_string = (char **) malloc ((ft_strlen(s) * ft_strlen(s) + 1) * sizeof(char **));
+	if (!r_string)
+		return (NULL);
+	while ((size_t)i <= ft_strlen(s) + 1)
+	{
+		r_string[i] = (char *) malloc ((ft_strlen(s) + 1));
+		if (!r_string[i])
+		{
+			ft_free_all(r_string, i);
+			return (NULL);
+		}
+		i++;
+	}
+	ft_bzero(r_string[0], 1);
+	return (r_string);
+}
 
 char	**ft_split(char const *s, char c)
 {
@@ -21,49 +61,7 @@ char	**ft_split(char const *s, char c)
 	int		j;
 	int		k;
 
-	r_string = (char **) malloc (9000000);
-	r_string[0] = (char *) malloc (5000);
-	r_string[1] = (char *) malloc (5000);
-	r_string[2] = (char *) malloc (5000);
-	r_string[3] = (char *) malloc (5000);
-	r_string[4] = (char *) malloc (5000);
-	r_string[5] = (char *) malloc (5000);
-	r_string[6] = (char *) malloc (5000);
-	r_string[7] = (char *) malloc (5000);
-	r_string[8] = (char *) malloc (5000);
-	r_string[9] = (char *) malloc (5000);
-	r_string[10] = (char *) malloc (5000);
-	r_string[11] = (char *) malloc (5000);
-	r_string[12] = (char *) malloc (5000);
-	r_string[13] = (char *) malloc (5000);
-	r_string[14] = (char *) malloc (5000);
-	r_string[15] = (char *) malloc (5000);
-	r_string[16] = (char *) malloc (5000);
-	r_string[17] = (char *) malloc (5000);
-	r_string[18] = (char *) malloc (5000);
-	r_string[19] = (char *) malloc (5000);
-	r_string[20] = (char *) malloc (5000);
-	r_string[21] = (char *) malloc (5000);
-	r_string[22] = (char *) malloc (5000);
-	r_string[23] = (char *) malloc (5000);
-	r_string[24] = (char *) malloc (5000);
-	r_string[25] = (char *) malloc (5000);
-	r_string[26] = (char *) malloc (5000);
-	r_string[27] = (char *) malloc (5000);
-	r_string[28] = (char *) malloc (5000);
-	r_string[29] = (char *) malloc (5000);
-	r_string[30] = (char *) malloc (5000);
-	r_string[31] = (char *) malloc (5000);
-	r_string[32] = (char *) malloc (5000);
-	r_string[33] = (char *) malloc (5000);
-	r_string[34] = (char *) malloc (5000);
-	r_string[35] = (char *) malloc (5000);
-	r_string[36] = (char *) malloc (5000);
-	r_string[37] = (char *) malloc (5000);
-	r_string[38] = (char *) malloc (5000);
-	r_string[39] = (char *) malloc (5000);
-	r_string[40] = (char *) malloc (5000);
-	ft_bzero(r_string[0], 1);
+	r_string = ft_malloced(s);
 	if (!r_string)
 		return (NULL);
 	i = 0;
@@ -82,7 +80,7 @@ char	**ft_split(char const *s, char c)
 		if (s[i] == c && s[i + 1] != c)
 		{
 			i++;
-			if (ft_strlen((const char *)r_string[0]) != 0)
+			if (ft_isempty(r_string[0]) != 0)
 			{
 				r_string[k][j] = '\0';
 				k++;
@@ -100,7 +98,7 @@ char	**ft_split(char const *s, char c)
 #include <stdio.h>
 int	main(void)
 {
-	char	**splitted = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ');
+	char	**splitted = ft_split("test ", ' ');
 	int i = 0;
 	while (splitted[i] != NULL)
 	{
