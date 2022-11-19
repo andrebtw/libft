96e07dbbf9a60bc25c6d4482f4459ab753e3500f
André <43089870+andrebtw@users.noreply.github.com>
2022-11-19 17:31:40 +0000

    Remade strtrim, no memory leaks now :D

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,103 +14,96 @@
 #include <string.h>
 #include "libft.h"
 
-static int	ft_strlen_int(const char *s)
+static int	ft_isinchar(char const *string, char c)
 {
-	int	index;
+	int	i;
 
-	index = 0;
-	while (s[index] != '\0')
-		index++;
-	return (index);
+	i = 0;
+	while (string[i])
+	{
+		if (string[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-static char	*ft_r_string(const char *s)
+static int	ft_skipbegin(char const *string, char const *set, int s1_index)
 {
+	int	start;
+
+	start = 0;
+	if (ft_isinchar(set, string[s1_index]))
+	{
+		while (ft_isinchar(set, string[s1_index]))
+		{
+			s1_index++;
+			start++;
+		}
+	}
+	return (start);
+}
+
+static int	ft_skipend(char const *string, char const *set, int	s1_index, int end)
+{
+	while (string[s1_index])
+	{
+		end = 0;
+		while (ft_isinchar(set, string[s1_index]) && string[s1_index])
+		{
+			end++;
+			s1_index++;
+		}
+		if (!string[s1_index])
+			return (end);
+		s1_index++;
+	}
+	return (0);
+}
+
+static char	*ft_r_string(char const *s1, int start, int end)
+{
+	int		i;
+	int		j;
 	char	*r_string;
 
-	r_string = ft_strdup(s);
+	r_string = (char *) malloc ((ft_strlen(s1) + 1) - (start + end));
+	if (!r_string)
+		return (NULL);
+	i = start;
+	j = 0;
+	while (s1[i] && j < (int)ft_strlen(s1) - ((start + end)))
+	{
+		r_string[j] = s1[i];
+		j++;
+		i++;
+	}
+	r_string[j] = '\0';
 	return (r_string);
-}
-
-static int	ft_skip_begin(char const *s1, char const *set)
-{
-	int	i;
-	int	j;
-	int	found_char;
-
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		found_char = 0;
-		j = 0;
-		while (set[j] != '\0')
-		{
-			if (s1[i] == set[j])
-				found_char = 1;
-			j++;
-		}
-		if (found_char == 0)
-			return (i);
-		i++;
-	}
-	return (i);
-}
-
-static int	ft_skip_end(char const *s1, char const *set, int count)
-{
-	int	i;
-	int	j;
-	int	found_char;
-
-	i = 0;
-	while (s1[count + i] != '\0')
-	{
-		found_char = 0;
-		j = 0;
-		while (set[j] != '\0')
-		{
-			if (s1[count + i] == set[j])
-				found_char = 1;
-			j++;
-		}
-		if (found_char == 0)
-			return (count);
-		i++;
-	}
-	if (s1[count + i] == '\0')
-		return (count + i);
-	return (count);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		k;
+	int		start;
+	int		end;
+	int		s1_index;
 	char	*r_string;
 
-	r_string = (char *) malloc (ft_strlen_int(s1) + 1);
-	if (r_string == NULL)
-		return (NULL);
-	i = 0;
-	k = 0;
-	while (s1[i] != '\0')
+	if (!set[0])
+		return (ft_strdup(s1));
+	s1_index = 0;
+	start = ft_skipbegin(s1, set, s1_index);
+	end = ft_skipend(s1, set, s1_index, start);
+	if (!s1[0] || start + end >= (int)ft_strlen(s1))
 	{
-		if (i == 0)
-			i = ft_skip_begin(s1, set);
-		i = ft_skip_end(s1, set, i);
-		r_string[k] = s1[i];
-		k++;
-		if (s1[i] == '\0')
-			return (ft_r_string(r_string));
-		i++;
+		r_string = (char *) malloc (1);
+		if (!r_string)
+			return (NULL);
+		r_string[0] = '\0';
+		return (r_string);
 	}
-	r_string[k] = '\0';
-	return (ft_r_string(r_string));
+	r_string = ft_r_string(s1, start, end);
+	if (!r_string)
+		return (NULL);
+	return (r_string);
 }
-/*
-#include <stdio.h>
-int	main(void)
-{
-	printf("%s", ft_strtrim("lorem ipsum dolor sit amet", "te"));
-	return (0);
-}*/
