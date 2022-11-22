a5b66d5cd25ebdc4c8b89c508d56d58721ac4747
André <gwandrewgd@gmail.com>
2022-11-23 00:12:05 +0100

    Fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:44:57 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/11/22 15:04:18 by anrodri2         ###   ########.fr       */
+/*   Updated: 2022/11/23 00:10:56 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,14 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	count = 1;
-	size = 1;
-	/*
 	unsigned char	*array;
 	size_t			i;
 	size_t			k;
 
 	k = 0;
-	if (SIZE_MAX / size <= count)
-		return (NULL);
+	if (size)
+		if (SIZE_MAX / size <= count)
+			return (NULL);
 	array = (unsigned char *) malloc ((count * size));
 	if (array == NULL)
 		return (NULL);
@@ -35,6 +33,5 @@ void	*ft_calloc(size_t count, size_t size)
 		array[k] = '\0';
 		k++;
 	}
-	return ((void *)array);*/
-	return (NULL);
+	return ((void *)array);
 }
