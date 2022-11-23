eba431db9ef17ca5a9636b7da3b8f82d457eca14
André <gwandrewgd@gmail.com>
2022-11-23 01:02:14 +0100

    Fixed ft_atoi, now works like the real atoi for number superior to long and -long.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 10:37:31 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/11/08 13:57:17 by anrodri2         ###   ########.fr       */
+/*   Updated: 2022/11/23 00:58:44 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 static int	ft_isspace(char character)
 {
@@ -18,15 +20,41 @@ static int	ft_isspace(char character)
 	return (0);
 }
 
+static int	ft_check_if_of(long long number, int neg_mult, size_t nb_length)
+{
+	if (!number)
+		return (0);
+	if (nb_length >= 20)
+	{
+		if (neg_mult == 1)
+			return (-1);
+		else
+			return (0);
+	}
+	if (neg_mult == 1)
+	{
+		if ((unsigned long long)number >= __LONG_LONG_MAX__)
+			return (-1);
+	}
+	else
+	{
+		if ((unsigned long long)number - 1 >= __LONG_LONG_MAX__)
+			return (0);
+	}
+	return (number * neg_mult);
+}
+
 int	ft_atoi(const char *nptr)
 {
-	int	number;
-	int	neg_mult;
-	int	i;
+	long long		number;
+	int				neg_mult;
+	int				i;
+	size_t			nb_length;
 
 	i = 0;
 	number = 0;
 	neg_mult = 1;
+	nb_length = 0;
 	while (ft_isspace(nptr[i]) == 1)
 		i++;
 	if (nptr[i] == '+')
@@ -38,10 +66,9 @@ int	ft_atoi(const char *nptr)
 	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		number = number * 10 + nptr[i] - '0';
-		i++;
+		number = number * 10 + nptr[i++] - '0';
+		if (nptr[i] != '0')
+			nb_length++;
 	}
-	if (number == 0)
-		return (0);
-	return (number * neg_mult);
+	return (ft_check_if_of(number, neg_mult, nb_length));
 }
